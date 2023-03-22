847e7361b2d16170ab211fe3f548699d6e113a1e
Elisa Diaz <elisadiazartcontact@gmail.com>
2023-03-22 21:54:10 +0100

    22.03.23

--- src/components/list-item/ListItem.jsx
+++ src/components/list-item/ListItem.jsx
@@ -2,11 +2,11 @@ import styles from './list-item.module.scss'
 
 const ListItem = ({image, number, title, text}) => {
     return <div className={styles['list-item']}>
-        <img src={image} alt="" />
+        <img className={styles['list-item__image']} src={image} alt="" />
         <div className={styles['list-item__text-container']}>
-            <h2>{number}</h2>
-            <h3>{title}</h3>
-            <p>{text}</p>
+            <h2 className={styles['list-item__number']}>{number}</h2>
+            <h3 className={styles['list-item__title']}>{title}</h3>
+            <p className={styles['list-item__text']}>{text}</p>
         </div>
     </div>
 }
